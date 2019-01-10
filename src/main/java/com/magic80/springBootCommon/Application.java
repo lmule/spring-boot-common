2e273aa84e3a10f05a1b63857bb80bdf9faa4263
dingrui <dingrui@daojia-inc.com>
2019-01-10 15:45:14 +0800

    init

--- src/main/java/com/magic80/springBootCommon/Application.java
+++
@@ -1,9 +0,0 @@
-package com.magic80.springBootCommon;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-    }
-}
